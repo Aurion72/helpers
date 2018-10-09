bf756bc42e5c364f18bc382799de66eedd27f80c
toyi <anthony.aslangul@gmail.com>
2018-10-09 21:22:25 +0200

    loadindTimeEnd: a second argument allow to override the loadingTimeStart

--- src/HelpersDebug.php
+++ src/HelpersDebug.php
@@ -55,14 +55,15 @@ class HelpersDebug
      * Save loading time
      *
      * @param bool $die
+     * @param float|null $override_start_microtime
      * @return int|mixed
      */
-    public static function loadingTimeEnd($die = true)
+    public static function loadingTimeEnd($die = true, float $override_start_microtime = null)
     {
-        $result = microtime(true) - static::$start_microtime;
-        if ($die) dd($result);
+        $start_microtime = $override_start_microtime ?: static::$start_microtime;
+        $result = microtime(true) - $start_microtime;
+        if ($die) dd($result); 
 
         return $result;
     }
-
 }
